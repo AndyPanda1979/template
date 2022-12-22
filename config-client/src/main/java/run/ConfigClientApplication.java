1cb9af80c0ca60bdaae356126f1803f0f15e978d
Andrei Kalinichenko <andypanda197907@gmail.com>
2022-12-22 23:31:21 +0300

    Разбираемся с конфигурационными серверами...

---
+++ config-client/src/main/java/run/ConfigClientApplication.java
@@ -0,0 +1,17 @@
+package run;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+}
+
+
+//TODO
+//Надо разобраться с настройками, пока странно запускается...
